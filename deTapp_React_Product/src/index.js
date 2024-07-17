3616c32d42db6b552ec6fd929aee01b3af7bf0f7
karthikeyandhanasekar <dkkarthik2000@gmail.com>
2024-07-17 07:45:07 +0530

    HashRouter instead of BrowserRouter

--- deTapp_React_Product/src/index.js
+++ deTapp_React_Product/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Suspense>
-      <BrowserRouter>
+      <HashRouter basename="/">
           <App />
-      </BrowserRouter>
+      </HashRouter>
     </Suspense>
   </React.StrictMode>
 );
