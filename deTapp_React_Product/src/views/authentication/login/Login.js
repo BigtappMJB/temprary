9a604378635c1dd7d6f762096af9c4ae83306036
karthikeyandhanasekar <dkkarthik2000@gmail.com>
2024-07-22 14:15:32 +0530

    MultiFactor Authentication

--- deTapp_React_Product/src/views/authentication/login/Login.js
+++ deTapp_React_Product/src/views/authentication/login/Login.js
@@ -94,20 +94,42 @@ const LoginPage = () => {
     });
   };
 
+  /**
+   * Triggers the sending of an OTP email.
+   *
+   * This function calls the `triggerOTPEmailController` to send an OTP email to the user. Upon success,
+   * it sets a cookie indicating that the user should be redirected to the dashboard, and navigates
+   * the user to the email verification page. If an error occurs during the process, it logs the error
+   * to the console.
+   *
+   * @example
+   * // Call this function to trigger the OTP email and handle navigation
+   * triggerOTPEmail()
+   *   .then(() => {
+   *     console.log('OTP email triggered successfully and user redirected to email verification page.');
+   *   })
+   *   .catch((error) => {
+   *     console.error('Failed to trigger OTP email:', error);
+   *   });
+   *
+   * @returns {Promise<void>} A promise that resolves when the OTP email process is complete.
+   */
   const triggerOTPEmail = async () => {
     try {
-      startLoading();
+      // Call the controller to trigger the OTP email
       await triggerOTPEmailController();
+
+      // Set a cookie indicating that the user should be redirected to the dashboard
       setCookie({
         name: isDashboardRedirectCookieName,
         value: encodeData(1),
       });
 
+      // Navigate the user to the email verification page
       navigate("/auth/emailVerification");
     } catch (error) {
-      console.error(error);
-    } finally {
-      // stopLoading();
+      // Log any errors that occur during the OTP email process
+      console.error("Error triggering OTP email:", error);
     }
   };
 
@@ -184,7 +206,7 @@ const LoginPage = () => {
           value: encodeData(1),
         });
 
-        triggerOTPEmail();
+        await triggerOTPEmail();
         // Log the successful login and navigate to the dashboard
       }
     } catch (error) {
