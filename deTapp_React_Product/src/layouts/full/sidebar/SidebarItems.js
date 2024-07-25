60d065ba5314d5a9af1c6bc15775f7c7e726d20e
karthikeyandhanasekar <dkkarthik2000@gmail.com>
2024-07-25 14:09:10 +0530

    Sidebar

--- deTapp_React_Product/src/layouts/full/sidebar/SidebarItems.js
+++ deTapp_React_Product/src/layouts/full/sidebar/SidebarItems.js
@@ -9,21 +9,39 @@ const SidebarItems = ({ navItemClicked }) => {
   const { menuList } = useLoginProvider();
   // Transform API data to match the required structure
   const transformApiData = (data) => {
-    return data && data?.map((menu, index) => ({
-      id: index,
-      subheader: menu.menu_name,
-      children: menu.submenus.map((submenu, subIndex) => ({
-        id: `${index}-${subIndex}`,
-        title: submenu.submenu_name,
-        href: submenu.submenu_path,
-        icon: People, // Adjust the icon as per your needs
-      })),
-    }));
+    return (
+      data &&
+      data?.map((menu, index) => ({
+        id: index,
+        subheader: menu.menu_name,
+        children: menu.submenus.map((submenu, subIndex) => ({
+          id: `${index}-${subIndex}`,
+          title: submenu.submenu_name,
+          href: submenu.submenu_path,
+          icon: People, // Adjust the icon as per your needs
+        })),
+      }))
+    );
+  };
+  const dashobardSubItem = {
+    id: -1,
+    title: "Dashboard",
+    href: "/dashboard",
+    icon: People,
   };
   const MenuItems = transformApiData(menuList);
   return (
     <Box sx={{ px: 3 }}>
       <List sx={{ pt: 0 }} className="sidebarNav">
+        <React.Fragment key={dashobardSubItem.id}>
+          <NavGroup item={"Dashboard"} />
+
+          <NavItem
+            onClick={navItemClicked}
+            item={dashobardSubItem}
+            key={dashobardSubItem.id}
+          />
+        </React.Fragment>
         {MenuItems.map((item) => (
           <React.Fragment key={item.id}>
             <NavGroup item={item} />
