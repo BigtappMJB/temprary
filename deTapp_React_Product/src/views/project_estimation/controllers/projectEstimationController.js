f15de2be20ffa92d9c6e50510d508749bd297db9
karthikeyandhanasekar <dkkarthik2000@gmail.com>
2024-09-09 12:23:14 +0530

    added working hours

--- deTapp_React_Product/src/views/project_estimation/controllers/projectEstimationController.js
+++ deTapp_React_Product/src/views/project_estimation/controllers/projectEstimationController.js
@@ -127,6 +127,8 @@ export const projectEstimateCreationController = async (formData) => {
       throw new Error("Invalid form data");
     }
 
+    // console.log({formData});
+
     debugger;
 
     const email = decodeData(getCookie(isUserIdCookieName));
@@ -141,6 +143,7 @@ export const projectEstimateCreationController = async (formData) => {
       endDate: moment(formData.endDate).format("YYYY-MM-DD"),
       noOfHours: Number(formData.hoursPerDay),
       totalHours: Number(formData?.totalHours),
+      workingDays: Number(formData?.workingDays),
       createdBy: email,
       isActive: 1,
     };
@@ -200,6 +203,8 @@ export const projectEstimateUpdateController = async (formData) => {
       endDate: moment(formData.endDate).format("YYYY-MM-DD"),
       noOfHours: Number(formData.hoursPerDay),
       totalHours: Number(formData?.totalHours),
+      workingDays: Number(formData?.workingDays),
+
       // created_by: email,
       // is_active: true,
     };
