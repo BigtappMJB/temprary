1d9abaaddc1e52be9c3034b8598e1a36619d80aa
BigtappMJB <mohammad.juned@bigtappanalytics.com>
2025-05-28 11:42:00 +0530

    Delete Rapid_Code_Backened directory
    Added it in separate repo

--- Rapid_Code_Backened/run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Building and running Rapid Code Backend...
-call mvn clean install
-if %ERRORLEVEL% NEQ 0 (
-    echo Build failed!
-    exit /b %ERRORLEVEL%
-)
-echo Build successful! Starting application...
-call mvn spring-boot:run
--- Rapid_Code_Backened/test-generate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Testing code generation API...
-
-curl -X POST http://localhost:8080/api/generate ^
-  -H "Content-Type: application/json" ^
-  -d "{\"projectName\":\"StudentManagement\",\"packageName\":\"com.example.student\",\"modules\":[{\"type\":\"entity\",\"name\":\"Student\",\"fields\":[{\"name\":\"id\",\"type\":\"Long\"},{\"name\":\"name\",\"type\":\"String\"},{\"name\":\"email\",\"type\":\"String\"}]},{\"type\":\"controller\",\"name\":\"StudentController\"}]}"
-
-echo.
-echo Test completed.
