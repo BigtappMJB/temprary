5d3b604459aef2b61e3cc85cb05c59f6fc7c10a5
BigtappMJB <mohammad.juned@bigtappanalytics.com>
2025-07-01 21:14:55 +0530

    Delete ClaimsAutomationAngular directory

--- ClaimsAutomationAngular/src/app/shared/services/data-storage.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class DataStorageService {
-  public isUserLoggedIn: boolean = false;
-  public MenuList: any;
-}
